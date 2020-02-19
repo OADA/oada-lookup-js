f72f38d940fad02ba08fcd44f34319ece52ebd95
Alex Layton <alex@layton.in>
2020-02-19 17:26:51 -0500

    Replace old lint stuff

--- lookup.js
+++ lookup.js
@@ -12,175 +12,201 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
+'use strict'
 
-var url = require('url');
-var request = require('superagent');
-var OADAError = require('oada-error').OADAError;
+var url = require('url')
+var request = require('superagent')
+var OADAError = require('oada-error').OADAError
 
 // Fetch a Well-Known (RFC 5785) Resource. The hostname will automatically be
 // parsed from any URL.
-function wellKnown(hostname, suffix, options, cb) {
+function wellKnown (hostname, suffix, options, cb) {
   // Check for options object
   if (typeof options === 'function') {
-    cb = options;
-    options = {};
+    cb = options
+    options = {}
   }
 
   // Default options
-  options.timeout = options.timeout || 1000;
+  options.timeout = options.timeout || 1000
 
   // Parse hostname for just hostname parts
-  var parts = url.parse(hostname, false, true);
+  var parts = url.parse(hostname, false, true)
 
   // Typical use: hostname = agcloud.com --> parts.path == "agcloud.com"...
   if (!parts.hostname) {
-    parts.hostname = parts.path;
+    parts.hostname = parts.path
   }
-  parts.protocol = 'https';
-  parts.pathname = '/.well-known/' + suffix;
-  parts.path = null;
-  parts.search = null;
-  parts.query = null;
-  parts.hash = null;
+  parts.protocol = 'https'
+  parts.pathname = '/.well-known/' + suffix
+  parts.path = null
+  parts.search = null
+  parts.query = null
+  parts.hash = null
 
   request
     .get(url.format(parts))
     .accept('application/json')
     .timeout(options.timeout)
-    .end(function(err, res) {
-      if (err) { return cb(err); }
+    .end(function (err, res) {
+      if (err) {
+        return cb(err)
+      }
 
       // Makre sure request was OK and that it was valid JSON (body was parsed)
       if (res.ok && Object.keys(res.body).length) {
-        cb(null, res.body);
+        cb(null, res.body)
       } else if (res.ok) {
-        cb(new Error('Invalid ' + suffix + ' Well-Known'));
+        cb(new Error('Invalid ' + suffix + ' Well-Known'))
       } else {
-        cb(new OADAError(res.error.text,
+        cb(
+          new OADAError(
+            res.error.text,
             res.error.status,
             suffix + ' at ' + hostname + ' not found',
-            res.error.path));
+            res.error.path
+          )
+        )
       }
-    });
+    })
 }
 
 // Fetch a client registration from an OADA client id.
-function clientRegistration(clientId, options, cb) {
+function clientRegistration (clientId, options, cb) {
   // Check for options object
   if (typeof options === 'function') {
-    cb = options;
-    options = {};
+    cb = options
+    options = {}
   }
 
   // Default options
-  options.timeout = options.timeout || 1000;
+  options.timeout = options.timeout || 1000
 
   // Parse client id
-  var cid = clientId.split('@');
-  if (cid.length != 2) {
-    return cb(new Error('Invalid clientId'));
+  var cid = clientId.split('@')
+  if (cid.length !== 2) {
+    return cb(new Error('Invalid clientId'))
   }
 
-  var host = 'https://' + cid[1];
+  var host = 'https://' + cid[1]
 
   // Get the OADA configuration of where the client is hosted
-  wellKnown(host, 'oada-client-discovery',  options, function(err, conf) {
-    if (err) { return cb(err); }
+  wellKnown(host, 'oada-client-discovery', options, function (err, conf) {
+    if (err) {
+      return cb(err)
+    }
 
     // Verify the host support client discovery
     if (!conf['client_discovery']) {
-      return cb(new Error('Host does not support client discovery'));
+      return cb(new Error('Host does not support client discovery'))
     }
 
     // Discover the clients registration
     request
       .get(conf['client_discovery'])
-      .query({'clientId': clientId})
+      .query({ clientId: clientId })
       .accept('applcation/json')
       .timeout(options.timeout)
-      .end(function(err, res) {
-        if (err) { return cb(err); }
+      .end(function (err, res) {
+        if (err) {
+          return cb(err)
+        }
 
         if (res.ok && Object.keys(res.body).length) {
-          cb(null, res.body);
+          cb(null, res.body)
         } else if (res.ok) {
-          cb(new Error('Invalid client registration'));
+          cb(new Error('Invalid client registration'))
         } else {
-          cb(new OADAError(res.error.text,
-                           res.error.status,
-                           'Client not found',
-                           res.error.path));
+          cb(
+            new OADAError(
+              res.error.text,
+              res.error.status,
+              'Client not found',
+              res.error.path
+            )
+          )
         }
-      });
-  });
+      })
+  })
 }
 
 // Fetch a JSON Web Key Set
-function jwks(uri, options, cb) {
+function jwks (uri, options, cb) {
   // Check for options object
   if (typeof options === 'function') {
-    cb = options;
-    options = {};
+    cb = options
+    options = {}
   }
 
   // Default options
-  options.timeout = options.timeout || 1000;
+  options.timeout = options.timeout || 1000
 
   // Get JWKs document
   request
     .get(uri)
     .accept('application/json')
     .timeout(options.timeout)
-    .end(function(err, res) {
-      if (err) { return cb(err); }
+    .end(function (err, res) {
+      if (err) {
+        return cb(err)
+      }
 
       if (res.ok && Object.keys(res.body).length && res.body.keys) {
-        cb(null, res.body);
+        cb(null, res.body)
       } else if (res.ok) {
-        cb(new Error('Invalid JWKs document'));
+        cb(new Error('Invalid JWKs document'))
       } else {
-        cb(new OADAError(res.error.text,
-                         res.error.status,
-                         'Client public keys not found',
-                         res.error.path));
+        cb(
+          new OADAError(
+            res.error.text,
+            res.error.status,
+            'Client public keys not found',
+            res.error.path
+          )
+        )
       }
-    });
+    })
 }
 
 // Fetch trsuted client discovery providers list
-function trustedCDP(options, cb) {
+function trustedCDP (options, cb) {
   // Check for options object
   if (typeof options === 'function') {
-    cb = options;
-    options = {};
+    cb = options
+    options = {}
   }
 
   // Default options
-  options.timeout = options.timeout || 1000;
+  options.timeout = options.timeout || 1000
 
   // Get trusted CDP list
   request
     .get('https://oada.github.io/oada-trusted-lists/client-discovery.json')
     .accept('text/plain')
     .timeout(options.timeout)
-    .end(function(err, res) {
-      if (err) { return cb(err); }
+    .end(function (err, res) {
+      if (err) {
+        return cb(err)
+      }
 
       if (res.ok && res.body.length) {
-        cb(null, res.body);
+        cb(null, res.body)
       } else if (res.ok) {
-        cb(new Error('Invalid trusted client discovery provider list'));
+        cb(new Error('Invalid trusted client discovery provider list'))
       } else {
-        cb(new OADAError(res.error.text,
-                         res.error.status,
-                         'Trusted client discovery provider list not found',
-                         res.error.path));
+        cb(
+          new OADAError(
+            res.error.text,
+            res.error.status,
+            'Trusted client discovery provider list not found',
+            res.error.path
+          )
+        )
       }
-    });
+    })
 }
 
-module.exports.wellKnown = wellKnown;
-module.exports.clientRegistration = clientRegistration;
-module.exports.jwks = jwks;
-module.exports.trustedCDP = trustedCDP;
+module.exports.wellKnown = wellKnown
+module.exports.clientRegistration = clientRegistration
+module.exports.jwks = jwks
+module.exports.trustedCDP = trustedCDP
--- test/lookup.test.js
+++ test/lookup.test.js
@@ -15,348 +15,346 @@
  */
 /*global describe, it, afterEach, beforeEach */
 /*jshint expr:true */
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
-var nock = require('nock');
-var lookup = require('../lookup');
+var expect = require('chai').expect
+var nock = require('nock')
+var lookup = require('../lookup')
 
-var oadaConfig = require('./sample/oada-configuration');
-var oadaClientDiscovery = require('./sample/oada-client-discovery');
-var clientReg = require('./sample/client-registration');
-var jwks = require('./sample/jwks');
-var trustedCDP = require('./sample/client-discovery');
+var oadaConfig = require('./sample/oada-configuration')
+var oadaClientDiscovery = require('./sample/oada-client-discovery')
+var clientReg = require('./sample/client-registration')
+var jwks = require('./sample/jwks')
+var trustedCDP = require('./sample/client-discovery')
 
-var mockUrl = 'https://oada.local';
-var mockHost = 'oada.local';
+var mockUrl = 'https://oada.local'
+var mockHost = 'oada.local'
 
-describe('lookup', function() {
+describe('lookup', function () {
+  describe('#wellKnown', function () {
+    afterEach(function () {
+      nock.cleanAll()
+    })
 
-  describe('#wellKnown', function() {
+    it('should be exported', function () {
+      expect(lookup.wellKnown).to.be.a('function')
+    })
 
-    afterEach(function() {
-      nock.cleanAll();
-    });
-
-    it('should be exported', function() {
-      expect(lookup.wellKnown).to.be.a('function');
-    });
-
-    it('should fetch document', function(done) {
+    it('should fetch document', function (done) {
       nock(mockUrl)
         .get('/.well-known/oada-configuration')
-        .reply(200, oadaConfig);
+        .reply(200, oadaConfig)
 
-      lookup.wellKnown(mockHost, 'oada-configuration', function(err, conf) {
-        expect(err).to.not.be.ok;
-        expect(conf).to.deep.equal(oadaConfig);
+      lookup.wellKnown(mockHost, 'oada-configuration', function (err, conf) {
+        expect(err).to.not.be.ok
+        expect(conf).to.deep.equal(oadaConfig)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fetch document from URL', function(done) {
+    it('should fetch document from URL', function (done) {
       nock(mockUrl)
         .get('/.well-known/oada-configuration')
-        .reply(200, oadaConfig);
+        .reply(200, oadaConfig)
 
-      lookup.wellKnown('http://' + mockHost + '/a?b=c#d', 'oada-configuration',
-        function(err, conf) {
-          expect(err).to.not.be.ok;
-          expect(conf).to.deep.equal(oadaConfig);
+      lookup.wellKnown(
+        'http://' + mockHost + '/a?b=c#d',
+        'oada-configuration',
+        function (err, conf) {
+          expect(err).to.not.be.ok
+          expect(conf).to.deep.equal(oadaConfig)
 
-          done();
+          done()
         }
-      );
-    });
+      )
+    })
 
-    it('should fail if non-existent', function(done) {
+    it('should fail if non-existent', function (done) {
       nock(mockUrl)
         .get('/.well-known/oada-configuration')
-        .reply(404);
+        .reply(404)
 
-      lookup.wellKnown(mockHost, 'oada-configuration', function(err) {
-        expect(err.status).to.equal(404);
+      lookup.wellKnown(mockHost, 'oada-configuration', function (err) {
+        expect(err.status).to.equal(404)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if not valid', function(done) {
+    it('should fail if not valid', function (done) {
       nock(mockUrl)
         .get('/.well-known/oada-configuration')
-        .reply(200, 'Invalid Response');
+        .reply(200, 'Invalid Response')
 
-      lookup.wellKnown(mockHost, 'oada-configuration', function(err) {
-        expect(err).to.match(/Invalid oada-configuration Well-Known/);
+      lookup.wellKnown(mockHost, 'oada-configuration', function (err) {
+        expect(err).to.match(/Invalid oada-configuration Well-Known/)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail after timeout', function(done) {
-      var options = {
-        timeout: 10,
-      };
-
-      nock(mockUrl)
-        .get('/.well-known/oada-configuration')
-        .delayConnection(2 * options.timeout)
-        .reply(200, oadaConfig);
-
-      lookup.wellKnown(mockHost, 'oada-configuration', options, function(err) {
-        expect(err.timeout).to.equal(options.timeout);
-
-        done();
-      });
-    });
-  });
-
-  describe('#clientRegistration', function() {
-    var clientId = '123@oada.local';
-
-    beforeEach(function() {
-      nock(mockUrl)
-        .get('/.well-known/oada-client-discovery')
-        .reply(200, oadaClientDiscovery);
-    });
-
-    afterEach(function() {
-      nock.cleanAll();
-    });
-
-    it('should exported', function() {
-      expect(lookup.clientRegistration).to.be.a('function');
-    });
-
-    it('should fetch registration', function(done) {
-      nock(mockUrl)
-        .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
-        .get('/discover?clientId=XXX')
-        .reply(200, clientReg);
-
-      lookup.clientRegistration(clientId, function(err, config) {
-        expect(err).to.not.be.ok;
-        expect(config).to.deep.equal(clientReg);
-
-        done();
-      });
-    });
-
-    it('should fail if non-existent', function(done) {
-      nock(mockUrl)
-        .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
-        .get('/discover?clientId=XXX')
-        .reply(404);
-
-      lookup.clientRegistration(clientId, function(err) {
-        expect(err.status).to.equal(404);
-
-        done();
-      });
-    });
-
-    it('should fail if invalid', function(done) {
-      nock(mockUrl)
-        .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
-        .get('/discover?clientId=XXX')
-        .reply(200, 'Invalid Reponse');
-
-      lookup.clientRegistration(clientId, function(err) {
-        expect(err).to.match(/Invalid client registration/);
-
-        done();
-      });
-    });
-
-    it('should fail after timeout', function(done) {
+    it('should fail after timeout', function (done) {
       var options = {
         timeout: 10
-      };
+      }
+
+      nock(mockUrl)
+        .get('/.well-known/oada-configuration')
+        .delayConnection(2 * options.timeout)
+        .reply(200, oadaConfig)
+
+      lookup.wellKnown(mockHost, 'oada-configuration', options, function (err) {
+        expect(err.timeout).to.equal(options.timeout)
+
+        done()
+      })
+    })
+  })
+
+  describe('#clientRegistration', function () {
+    var clientId = '123@oada.local'
+
+    beforeEach(function () {
+      nock(mockUrl)
+        .get('/.well-known/oada-client-discovery')
+        .reply(200, oadaClientDiscovery)
+    })
+
+    afterEach(function () {
+      nock.cleanAll()
+    })
+
+    it('should exported', function () {
+      expect(lookup.clientRegistration).to.be.a('function')
+    })
+
+    it('should fetch registration', function (done) {
+      nock(mockUrl)
+        .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
+        .get('/discover?clientId=XXX')
+        .reply(200, clientReg)
+
+      lookup.clientRegistration(clientId, function (err, config) {
+        expect(err).to.not.be.ok
+        expect(config).to.deep.equal(clientReg)
+
+        done()
+      })
+    })
+
+    it('should fail if non-existent', function (done) {
+      nock(mockUrl)
+        .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
+        .get('/discover?clientId=XXX')
+        .reply(404)
+
+      lookup.clientRegistration(clientId, function (err) {
+        expect(err.status).to.equal(404)
+
+        done()
+      })
+    })
+
+    it('should fail if invalid', function (done) {
+      nock(mockUrl)
+        .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
+        .get('/discover?clientId=XXX')
+        .reply(200, 'Invalid Reponse')
+
+      lookup.clientRegistration(clientId, function (err) {
+        expect(err).to.match(/Invalid client registration/)
+
+        done()
+      })
+    })
+
+    it('should fail after timeout', function (done) {
+      var options = {
+        timeout: 10
+      }
 
       nock(mockUrl)
         .filteringPath(/clientId=[^&]*/g, 'clientId=XXX')
         .get('/discover?clientId=XXX')
         .delayConnection(2 * options.timeout)
-        .reply(200, clientReg);
+        .reply(200, clientReg)
 
-      lookup.clientRegistration(clientId, options, function(err) {
-        expect(err.timeout).to.equal(options.timeout);
+      lookup.clientRegistration(clientId, options, function (err) {
+        expect(err.timeout).to.equal(options.timeout)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if clientId is invalid', function(done) {
-      lookup.clientRegistration('123', function(err) {
-        expect(err).to.match(/Invalid clientId/);
+    it('should fail if clientId is invalid', function (done) {
+      lookup.clientRegistration('123', function (err) {
+        expect(err).to.match(/Invalid clientId/)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if provider does not support discovery', function(done) {
+    it('should fail if provider does not support discovery', function (done) {
       // Disable client discovery
-      var config = JSON.parse(JSON.stringify(oadaConfig));
-      delete config['client_discovery'];
-      nock.cleanAll();
+      var config = JSON.parse(JSON.stringify(oadaConfig))
+      delete config['client_discovery']
+      nock.cleanAll()
       nock(mockUrl)
         .get('/.well-known/oada-client-discovery')
-        .reply(200, config);
+        .reply(200, config)
 
-      lookup.clientRegistration(clientId, function(err) {
-        expect(err).to.match(/does not support client discovery/);
+      lookup.clientRegistration(clientId, function (err) {
+        expect(err).to.match(/does not support client discovery/)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if OADA configuration cannot be found', function(done) {
-      nock.cleanAll();
+    it('should fail if OADA configuration cannot be found', function (done) {
+      nock.cleanAll()
 
       nock(mockUrl)
         .get('/.well-known/oada-client-discovery')
-        .reply(404);
+        .reply(404)
 
       var options = {
         timeout: 10
-      };
+      }
 
-      lookup.clientRegistration(clientId, options, function(err) {
-        expect(err.status).to.equal(404);
+      lookup.clientRegistration(clientId, options, function (err) {
+        expect(err.status).to.equal(404)
 
-        done();
-      });
-    });
-  });
+        done()
+      })
+    })
+  })
 
-  describe('#jwks', function() {
+  describe('#jwks', function () {
+    afterEach(function () {
+      nock.cleanAll()
+    })
 
-    afterEach(function() {
-      nock.cleanAll();
-    });
+    it('should be exported', function () {
+      expect(lookup.jwks).to.be.a('function')
+    })
 
-    it('should be exported', function() {
-      expect(lookup.jwks).to.be.a('function');
-    });
-
-    it('should fetch document', function(done) {
+    it('should fetch document', function (done) {
       nock(mockUrl)
         .get('/jwks')
-        .reply(200, jwks);
+        .reply(200, jwks)
 
-      lookup.jwks(mockUrl + '/jwks', function(err, keys) {
-        expect(err).to.not.be.ok;
-        expect(keys).to.deep.equal(jwks);
+      lookup.jwks(mockUrl + '/jwks', function (err, keys) {
+        expect(err).to.not.be.ok
+        expect(keys).to.deep.equal(jwks)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if non-existent', function(done) {
+    it('should fail if non-existent', function (done) {
       nock(mockUrl)
         .get('/jwks')
-        .reply(404);
+        .reply(404)
 
-      lookup.jwks(mockUrl + '/jwks', function(err) {
-        expect(err.status).to.equal(404);
+      lookup.jwks(mockUrl + '/jwks', function (err) {
+        expect(err.status).to.equal(404)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if not valid', function(done) {
+    it('should fail if not valid', function (done) {
       nock(mockUrl)
         .get('/jwks')
-        .reply(200, 'Invalid Response');
+        .reply(200, 'Invalid Response')
 
-      lookup.jwks(mockUrl + '/jwks', function(err) {
-        expect(err).to.match(/Invalid JWKs document/);
+      lookup.jwks(mockUrl + '/jwks', function (err) {
+        expect(err).to.match(/Invalid JWKs document/)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail after timeout', function(done) {
+    it('should fail after timeout', function (done) {
       var options = {
-        timeout: 10,
-      };
+        timeout: 10
+      }
 
       nock(mockUrl)
         .get('/jwks')
         .delayConnection(2 * options.timeout)
-        .reply(200, jwks);
+        .reply(200, jwks)
 
-      lookup.jwks(mockUrl + '/jwks', options, function(err) {
-        expect(err.timeout).to.equal(options.timeout);
+      lookup.jwks(mockUrl + '/jwks', options, function (err) {
+        expect(err.timeout).to.equal(options.timeout)
 
-        done();
-      });
-    });
-  });
+        done()
+      })
+    })
+  })
 
-  describe('#trustedCDP', function() {
+  describe('#trustedCDP', function () {
+    afterEach(function () {
+      nock.cleanAll()
+    })
 
-    afterEach(function() {
-      nock.cleanAll();
-    });
+    it('should be exported', function () {
+      expect(lookup.trustedCDP).to.be.a('function')
+    })
 
-    it('should be exported', function() {
-      expect(lookup.trustedCDP).to.be.a('function');
-    });
-
-    it('should fetch document', function(done) {
+    it('should fetch document', function (done) {
       nock('https://oada.github.io')
         .get('/oada-trusted-lists/client-discovery.json')
-        .reply(200, trustedCDP);
+        .reply(200, trustedCDP)
 
-      lookup.trustedCDP(function(err, keys) {
-        expect(err).to.not.be.ok;
-        expect(keys).to.deep.equal(trustedCDP);
+      lookup.trustedCDP(function (err, keys) {
+        expect(err).to.not.be.ok
+        expect(keys).to.deep.equal(trustedCDP)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if non-existent', function(done) {
+    it('should fail if non-existent', function (done) {
       nock('https://oada.github.io')
         .get('/oada-trusted-lists/client-discovery.json')
-        .reply(404);
+        .reply(404)
 
-      lookup.trustedCDP(function(err) {
-        expect(err.status).to.equal(404);
+      lookup.trustedCDP(function (err) {
+        expect(err.status).to.equal(404)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail if not valid', function(done) {
+    it('should fail if not valid', function (done) {
       nock('https://oada.github.io')
         .get('/oada-trusted-lists/client-discovery.json')
-        .reply(200, 'Invalid Response');
+        .reply(200, 'Invalid Response')
 
-      lookup.trustedCDP(function(err) {
-        expect(err).to.match(/Invalid trusted client discovery provider list/);
+      lookup.trustedCDP(function (err) {
+        expect(err).to.match(/Invalid trusted client discovery provider list/)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
-    it('should fail after timeout', function(done) {
+    it('should fail after timeout', function (done) {
       var options = {
-        timeout: 10,
-      };
+        timeout: 10
+      }
 
       nock('https://oada.github.com')
         .get('/oada-trusted-lists/client-discovery.json')
         .delayConnection(2 * options.timeout)
-        .reply(200, trustedCDP);
+        .reply(200, trustedCDP)
 
-      lookup.trustedCDP(options, function(err) {
-        expect(err.timeout).to.equal(options.timeout);
+      lookup.trustedCDP(options, function (err) {
+        expect(err.timeout).to.equal(options.timeout)
 
-        done();
-      });
-    });
-  });
-});
+        done()
+      })
+    })
+  })
+})
